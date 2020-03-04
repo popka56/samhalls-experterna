fe19ea20522ea34f931541ec1841f85f372bafd7
Master Nara Lundmalm <52786849+Nartek01@users.noreply.github.com>
2020-03-04 10:29:32 +0100

    removed style and css-grid.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
+    <div class="header">
       <img class="logo" src="https://picsum.photos/200/300">
+      <Span>Xpert</Span>
      <a href="index.html"><img src="./assets/load-btn.svg" class="nav-btn"></a>
+     </div>
     <div id="nav"> <!--Här är vår globala header-->
       <router-link to="/hem">Hem</router-link> |
       <router-link to="/om">Om oss</router-link> |
@@ -9,26 +12,29 @@
       <router-link to="/sok">Sök</router-link> |
       <router-link to="/profil">Profil</router-link> |
     </div>
+    <div class="router-view">
     <router-view/>
+    </div>
     <!-- Footer -->
+    <footer>
     <div class="footerright">
       <!--Här är vår globala footer-->
         <ul>
           <li>
-            <a href="">Lorem-left</a>
+            <a href="">Lorem-right</a>
           </li>
           <li>
-            <a href="">Lorem-left</a>
+            <a href="">Lorem-right</a>
           </li>
           <li>
-            <a href="">Lorem-left</a>
+            <a href="">Lorem-right</a>
           </li>
           <li>
-            <a href="">Lorem-left</a>
+            <a href="">Lorem-right</a>
           </li>
         </ul>
     </div>
-    <div class="footerleft">
+    <!-- <div class="footerleft">
         <ul>
           <li>
             <a href="">Lorem-left</a>
@@ -43,7 +49,8 @@
             <a href="">Lorem-left</a>
           </li>
         </ul>
-    </div>  
+    </div>   -->
+    </footer>
   </div>
 </template>
 
@@ -57,58 +64,19 @@
 </style>
 
 <style scoped>
-* {
-  margin: 0;
-  padding: 0;
-}
 
-#app {
+.header {
   display: grid;
-  grid-template-columns: auto;
-  grid-template-rows: auto 100%;
-  grid-template-areas:
-    "logo nav-btn"
-    "nav nav"
-    "fl fr";
-}
-
-#nav {
-  grid-area: nav;
-  display: block;
-  background-color: rgba(196, 196, 196, 1);
-  height: 50px;
-}
-
-.footerright {
-  display: grid;
-  grid-area: fr;
-}
-
-.footerleft {
-  display: grid;
-  grid-area: fl;
-
 }
 
 .logo {
-  grid-area: logo;
-  height: 50px;
-  width: 100px;
+  width: auto;
+  height: 100px;
 }
 
-.nav-btn {
-  display: grid;
-  grid-area: nav-btn;
-  transform: rotate(90deg);
+footer {
+  background-color:#c0c0c0;
+  padding: 0;
+  margin: 0;
 }
-
-.nav-btn > img {
-  display: grid;
-  grid-area: nav-btn;
-  transform: rotate(90deg);
-}
-
- #app {
-   background-color: rgba(229, 229, 229, 1);
- }
 </style>
