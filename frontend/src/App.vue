151990d1948fc39903038af114a798aaa38755a7
Hugo Viklund <hugoviklund1@hotmail.com>
2020-03-18 11:31:25 +0100

    Gjort footer presenterbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -36,20 +36,8 @@
           <li>
             <a href="https://www.facebook.com/ITHogskolan"><i class="pr-3 fab fa-facebook-f"></i>ITHS-Facebook</a>
           </li>
-          <li>
-            <router-link to="/tos"><i class="pr-3 fas fa-file-alt"></i>Terms of Services</router-link>
-          </li>
-          <li>
-            <a href="https://www.google.com/maps/place/Ebbe+Lieberathsgatan+18C,+412+65+G%C3%B6teborg/@57.6794723,11.99917,17z/data=!3m1!4b1!4m5!3m4!1s0x464ff3ba575254c9:0x1026ff48d864454e!8m2!3d57.6794723!4d12.0013587"><i class="pr-3 fas fa-map-pin"></i>Ebbe Lieberathsgatan 18 c</a>
-          </li>
         </ul>
         <ul class="mb-0 p-3 list-unstyled">
-          <li>
-            <a href="https://www.iths.se/"><i class="pr-3 fas fa-book"></i>IT-Högskolan</a>
-          </li>
-          <li>
-            <a href="https://www.facebook.com/ITHogskolan"><i class="pr-3 fab fa-facebook-f"></i>ITHS-Facebook</a>
-          </li>
           <li>
             <router-link to="/tos"><i class="pr-3 fas fa-file-alt"></i>Terms of Services</router-link>
           </li>
